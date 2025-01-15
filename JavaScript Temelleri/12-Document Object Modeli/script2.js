bcc084a76cb3c0c85167e4fd7007d248266fd1aa
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-01-15 22:37:04 +0300

    DOM(Document Object Model) ileSelecting elemenler dersleri eklendi.

--- JavaScript Temelleri/12-Document Object Modeli/script2.js
+++ JavaScript Temelleri/12-Document Object Modeli/script2.js
@@ -23,16 +23,16 @@
 
 //*document.querySelector()
 
-console.log(document.querySelector("#header"));
-console.log(document.querySelector(".card-header"));//. başına koyulan nokta ile bulduğu ilk elemanı seçer.
-console.log(document.querySelector("div"));//ilk "div"i getirir.
+// console.log(document.querySelector("#header"));
+// console.log(document.querySelector(".card-header"));//. başına koyulan nokta ile bulduğu ilk elemanı seçer.
+// console.log(document.querySelector("div"));//ilk "div"i getirir.
 
-document.querySelector("li").style.color="red";//Bulduğu ilk "li" elemanını red yaptı.
-document.querySelector("li:last-child").style.color="blue";//son "li" elemanını mavi yapar.
-document.querySelector("li:nth-child(2)").style.color="green";//2. "li" elemanını yeşil yapar.
+// document.querySelector("li").style.color="red";//Bulduğu ilk "li" elemanını red yaptı.
+// document.querySelector("li:last-child").style.color="blue";//son "li" elemanını mavi yapar.
+// document.querySelector("li:nth-child(2)").style.color="green";//2. "li" elemanını yeşil yapar.
 
 
-document.querySelector("li:nth-child(3)").textContent="task item";//3. "li" elemanını textini değiştirir.
+// document.querySelector("li:nth-child(3)").textContent="task item";//3. "li" elemanını textini değiştirir.
 
 
 
@@ -40,6 +40,45 @@ document.querySelector("li:nth-child(3)").textContent="task item";//3. "li" elem
 
 //*** Multiple Elements
 
+//class name
+//document.getElementByClassName()
+
+let val;
+// val=document.getElementsByClassName("list-group-item");
+
+// //val=val[2];
+// val[2].style.color="green";
+// val[2].style.fontSize="25px";
+// val[1].textContent="new item";
+
+// for(let i=0;i<val.length;i++){
+//     val[i].style.color="red";
+//     val[i].textContent="Button"+(i+1);
+// }
+
+
+//document.getElementByTagName()
+
+// val=document.getElementsByTagName("li");
+// val=document.getElementById("task-list");
+// val=val.getElementsByTagName("a");
+
+
+//document.querySelectorAll()
+
+// val=document.querySelectorAll("li");
+
+// val.forEach(function(item,index){
+//     item.textContent=index + " - hello"
+// });
+
+val=document.querySelectorAll("li:nth-child(odd)");//li etiketlerinin tek sayılı indexlerini getirir.
+
+val.forEach(function(item){
+    item.style.background="#ccc"
+})
 
 
 
+
+console.log(val);
