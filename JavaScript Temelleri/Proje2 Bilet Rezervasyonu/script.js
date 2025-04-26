7a4f9528ab5b3017d1c2f412be1764c55e0d13f0
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-04-26 13:52:37 +0300

    Blet rezervasyon projesi için seçilen film için seçilen koltuk kadar sayı ile çarpılarak toplam fiyat hesaplandı. Gerekli fonksiyonlar yazıldı.

--- JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
+++ JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
@@ -1,6 +1,30 @@
 //Sinema Bilet Rezervasyonu
 
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const amount = document.getElementById("amount");
+const select = document.getElementById("movie");
 
+
+container.addEventListener("click", function(e){
+    if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved"))//tıklanan elemanın classında seat içeriyorsa ve reserved içermiyorsa şartı ekledik.
+    { 
+        e.target.classList.toggle("selected"); //selected classı varsa kaldırır yoksa ekler.
+        calculateTotal();
+        
+    }
+});
+
+select.addEventListener("change", function(e){
+    calculateTotal();
+});
+
+function calculateTotal(){
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;//container deki seat ve selected classına sahip nesnelerin sayısını verir.
+        let price = select.value;
+        count.innerText = selectedSeatCount;
+        amount.innerText = selectedSeatCount * price;
+}
 
 
 
