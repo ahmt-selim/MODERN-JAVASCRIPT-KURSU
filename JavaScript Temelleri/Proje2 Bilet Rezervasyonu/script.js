5f89b5408a6092ae6f438fc741a8d55f457f4130
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-04-26 14:14:09 +0300

    seçili filmin ve koltuk indexlerinin localStorage de kaydedilmesi sağlandı.

--- JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
+++ JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
@@ -4,7 +4,7 @@ const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
-
+const seats = document.querySelectorAll(".seat:not(.reserved)");//seat classına sahip ama reserved classına sahip olmayan nesneleri seçer.
 
 container.addEventListener("click", function(e){
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved"))//tıklanan elemanın classında seat içeriyorsa ve reserved içermiyorsa şartı ekledik.
@@ -20,13 +20,37 @@ select.addEventListener("change", function(e){
 });
 
 function calculateTotal(){
-    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;//container deki seat ve selected classına sahip nesnelerin sayısını verir.
+    const selectedSeats = container.querySelectorAll(".seat.selected");
+    
+    const selectedSeatsArr = [];
+    const seatsArr =[];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+    //[1,3,5]
+    let selectedSeatIndex = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+
+
+    let selectedSeatCount = selectedSeats.length;//container deki seat ve selected classına sahip nesnelerin sayısını verir.
         let price = select.value;
         count.innerText = selectedSeatCount;
         amount.innerText = selectedSeatCount * price;
+
+        saveToLocalStorage(selectedSeatIndex);
 }
 
-
+function saveToLocalStorage(indexs){
+    localStorage.setItem("selectedSeats", JSON.stringify(indexs));
+    localStorage.setItem("selectedMovieIndex", select.selectedIndex);
+}
 
 
 
