a24e2b40e57e3fb681f1e362dda2a1d73be98934
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-04-26 14:57:34 +0300

    Kaydet butonu ile seçili film ve koltukların kaydedilmesi sağlandı.

--- JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
+++ JavaScript Temelleri/Proje2 Bilet Rezervasyonu/script.js
@@ -6,6 +6,9 @@ const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
 const seats = document.querySelectorAll(".seat:not(.reserved)");//seat classına sahip ama reserved classına sahip olmayan nesneleri seçer.
 
+getFromLocalStorage();
+calculateTotal();
+
 container.addEventListener("click", function(e){
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved"))//tıklanan elemanın classında seat içeriyorsa ve reserved içermiyorsa şartı ekledik.
     { 
@@ -18,7 +21,7 @@ container.addEventListener("click", function(e){
 select.addEventListener("change", function(e){
     calculateTotal();
 });
-
+var selectedSeatIndex = [];
 function calculateTotal(){
     const selectedSeats = container.querySelectorAll(".seat.selected");
     
@@ -34,19 +37,42 @@ function calculateTotal(){
     });
 
     //[1,3,5]
-    let selectedSeatIndex = selectedSeatsArr.map(function(seat){
+    selectedSeatIndex = selectedSeatsArr.map(function(seat){
         return seatsArr.indexOf(seat);
     });
-
 
     let selectedSeatCount = selectedSeats.length;//container deki seat ve selected classına sahip nesnelerin sayısını verir.
         let price = select.value;
         count.innerText = selectedSeatCount;
         amount.innerText = selectedSeatCount * price;
 
-        saveToLocalStorage(selectedSeatIndex);
 }
 
+function getFromLocalStorage(){
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+    if(selectedSeats != null && selectedSeats.length > 0){
+        seats.forEach(function(seat, index){
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected");
+            }
+        });
+    }
+
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if(selectedMovieIndex != null) {
+        select.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
+const saveButton = document.getElementById("save");
+
+saveButton.addEventListener("click", function() {
+    saveToLocalStorage(selectedSeatIndex);
+});
 function saveToLocalStorage(indexs){
     localStorage.setItem("selectedSeats", JSON.stringify(indexs));
     localStorage.setItem("selectedMovieIndex", select.selectedIndex);
